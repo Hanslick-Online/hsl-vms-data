1377f413dfd111cd6504876f0c33407b8c047977
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2025-03-13 12:18:13 +0100

    fix xml:ids

--- scripts/sh/rename_files.sh
+++ scripts/sh/rename_files.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-for filename in $@; do
-	dirorig=`dirname ${filename}`
-	dirdest="`echo ${dirorig}|sed 's/_01_/_07_/g'|sed 's/_02_/_08_/g'`_renamed"
-	mkdir -p ${dirdest}
-	shortname=`basename $filename`
-	n=`echo "$shortname"|cut -d _ -f 1`
-	y=`echo "$shortname"|cut -d _ -f 3`
-	cp  ${filename}  "${dirdest}/VMS_Auflage_${n}_${y}.xml"
+orig_dir="$1"
+tmp_dir="tmp"
+dest_dir="`echo ${orig_dir} | sed 's/_01_/_07_/g'|sed 's/_02_/_08_/g'`_renamed"
+mkdir -p ${tmp_dir}
+mkdir -p ${dest_dir}
+cp ${orig_dir}/*.xml ${tmp_dir}
+
+for path in ${tmp_dir}/*; do
+	orig_file=`basename ${path}`
+	n=`echo "$orig_file"|cut -d _ -f 1`
+	y=`echo "$orig_file"|cut -d _ -f 3`
+	dest_file="VMS_Auflage_${n}_${y}.xml"
+	sed -i "s/t__$orig_file/$dest_file/g" ${tmp_dir}/*
+	cp  ${path}  "${tmp_dir}/${dest_file}"
 done
+cp ${tmp_dir}/VMS* ${dest_dir}
